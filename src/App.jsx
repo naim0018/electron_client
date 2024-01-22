0e91cecee189bcf8379b45e403bdaa3a7b2bb592
safkat07 <safkatsadli@gmail.com>
2024-01-23 03:03:01 +0600

    nested on hover  dropdown navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from './Pages/Header/Navbar/Navbar'
-import Home from './Pages/Home/Home'
-import  "./index.css"
+
+
+
+import "./index.css"
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
-      {/* <Home/> */}
     </div>
   )
 }
--- src/Pages/Header/Navbar/NavLinks.jsx
+++ src/Pages/Header/Navbar/NavLinks.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
 const NavLinks = () => {
+    const [isNestedDropdown1Open, setNestedDropdown1Open] = useState(false);
+    const [isNestedDropdown2Open, setNestedDropdown2Open] = useState(false);
+    const [isNestedDropdown3Open, setNestedDropdown3Open] = useState(false);
+    const [isNestedDropdown4Open, setNestedDropdown4Open] = useState(false);
     return (
-        <div className='lg:flex list-none font-poppins xl:text-base lg:text-[.9rem] hidden font-medium xl:gap-x-10 gap-x-5 text-white'>
+        <div className='lg:flex list-none font-poppins  xl:text-base lg:text-[.9rem] hidden font-medium xl:gap-x-12 gap-x-5 text-white'>
             {/* home */}
             <div className="relative inline-block group">
                 {/* Home */}
@@ -31,7 +38,7 @@ const NavLinks = () => {
             <div className="relative inline-block group">
                 {/* Home */}
                 <div className="cursor-pointer py-4 hover:text-yellow-200">
-                Categories
+                    Categories
                 </div>
                 {/* Dropdown Menu */}
                 <ul className="absolute hidden shadow-xl font-poppins overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-14 hover:block   font-normal   group-hover:block">
@@ -54,36 +61,102 @@ const NavLinks = () => {
                 </ul>
             </div>
             {/* products */}
+
+
             <div className="relative inline-block group">
                 {/* Home */}
                 <div className="cursor-pointer py-4 hover:text-yellow-200">
-                Products
+                    Products
                 </div>
                 {/* Dropdown Menu */}
-                <ul className="absolute hidden shadow-xl font-poppins overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-14 hover:block   font-normal   group-hover:block">
-                    <li className='cursor-pointer py-3 pl-3  hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
-                        Fashion 1
+                <ul className="absolute opacity-0 pointer-events-none dropdown-main group-hover:opacity-100 group-hover:pointer-events-auto  shadow-xl font-poppins list-none transition-all duration-700 text-gray-400 bg-white w-52 top-14 left-0 font-normal">
+                    <li
+                        onMouseEnter={() => setNestedDropdown1Open(true)}
+                        onMouseLeave={() => setNestedDropdown1Open(false)}
+                        className='cursor-pointer relative py-3 pl-3  group hover:opacity-100  hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                        Product Page
+                        <span className="text-2xl float-right mr-2 "><MdKeyboardArrowRight /></span>
                     </li>
-                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
-                        Fashion 2
-                    </li>
-                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
-                        Furniture
-                    </li>
-                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
-                        Mix Product
-                    </li>
-                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500'>
-                        Electronics
-                    </li>
+                    <ul className={`absolute  ${isNestedDropdown1Open ? 'block ' : 'hidden'} shadow-xl font-poppins  overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-0 left-52`}>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                            Product Left Sidebar
+                        </li>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500'>
+                            Product Right Sidebar
+                        </li>
+                    </ul>
 
+                    <li
+                        onMouseEnter={() => setNestedDropdown2Open(true)}
+                        onMouseLeave={() => setNestedDropdown2Open(false)}
+                        className='cursor-pointer relative py-3 pl-3  group hover:opacity-100  hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                        Product 360
+                        <span className="text-2xl float-right mr-2 "><MdKeyboardArrowRight /></span>
+                    </li>
+                    <ul className={`absolute  ${isNestedDropdown2Open ? 'block ' : 'hidden'} shadow-xl font-poppins  overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-11 left-52`}>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                            360t Left Sidebar
+                        </li>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500'>
+                            360 Right Sidebar
+                        </li>
+                    </ul>
+
+
+
+                    <li
+                        onMouseEnter={() => setNestedDropdown3Open(true)}
+                        onMouseLeave={() => setNestedDropdown3Open(false)}
+                        className='cursor-pointer relative py-3 pl-3  group hover:opacity-100  hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                        Product Video
+                        <span className="text-2xl float-right mr-2 "><MdKeyboardArrowRight /></span>
+                    </li>
+                    <ul className={`absolute  ${isNestedDropdown3Open ? 'block ' : 'hidden'} shadow-xl font-poppins  overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-24 left-52`}>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                            Video Left Sidebar
+                        </li>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500'>
+                            Video Right Sidebar
+                        </li>
+                    </ul>
+                    <li onMouseEnter={() => setNestedDropdown4Open(true)}
+                        onMouseLeave={() => setNestedDropdown4Open(false)}
+                        className='cursor-pointer relative py-3 pl-3  group hover:opacity-100  hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                        Product Gallery
+                        <span className="text-2xl float-right mr-2 "><MdKeyboardArrowRight /></span>
+                    </li>
+                    <ul className={`absolute  ${isNestedDropdown4Open ? 'block ' : 'hidden'} shadow-xl font-poppins  overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-36 left-52`}>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                            Video Left Sidebar
+                        </li>
+                        <li className='cursor-pointer w-full m-auto py-3 pl-3 hover:text-blue-500'>
+                            Video Right Sidebar
+                        </li>
+                    </ul>
+                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }} >
+                        Product Full Width
+                    </li>
+                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                        360 Full Width
+                    </li>
+                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                        Video Full Width
+                    </li>
+                    <li className='cursor-pointer py-3 pl-3 hover:text-blue-500' style={{ borderBottom: "1px solid #EEEEEE", width: "100%", marginLeft: "0" }}>
+                        Gallery Full Width
+                    </li>
                 </ul>
             </div>
+
+
+
+
+
             {/* pages */}
             <div className="relative inline-block group">
                 {/* Home */}
                 <div className="cursor-pointer py-4 hover:text-yellow-200">
-                Pages
+                    Pages
                 </div>
                 {/* Dropdown Menu */}
                 <ul className="absolute hidden shadow-xl font-poppins overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-14 hover:block   font-normal   group-hover:block">
@@ -109,7 +182,7 @@ const NavLinks = () => {
             <div className="relative inline-block group">
                 {/* Home */}
                 <div className="cursor-pointer py-4 hover:text-yellow-200">
-                Others
+                    Others
                 </div>
                 {/* Dropdown Menu */}
                 <ul className="absolute hidden shadow-xl font-poppins overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-14 hover:block   font-normal   group-hover:block">
@@ -135,7 +208,7 @@ const NavLinks = () => {
             <div className="relative inline-block group">
                 {/* Home */}
                 <div className="cursor-pointer py-4 hover:text-yellow-200">
-                Blog
+                    Blog
                 </div>
                 {/* Dropdown Menu */}
                 <ul className="absolute hidden shadow-xl font-poppins overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-14 hover:block   font-normal   group-hover:block">
@@ -161,7 +234,7 @@ const NavLinks = () => {
             <div className="relative inline-block group">
                 {/* Home */}
                 <div className="cursor-pointer py-4 hover:text-yellow-200">
-                Elements
+                    Elements
                 </div>
                 {/* Dropdown Menu */}
                 <ul className="absolute hidden shadow-xl font-poppins overflow-hidden transition-all duration-700 text-gray-400 bg-white w-52 top-14 hover:block   font-normal   group-hover:block">
