338bdaf43abe3c63dfdcf6511f7cf49a2cf09690
zinolot <mrjahidhassan0@gmail.com>
2024-01-23 21:44:20 +0600

    folder structure complete with navbar

---
+++ src/Router/MainRouter.jsx
@@ -0,0 +1,23 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import MainLayout from "../Layout/MainLayout";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+     
+    ],
+  }
+  
+]);
+
+export default router;
